13f42b7c1393d22bc306418afdcd18c850061abb
Caleb <blakeshawart@gmail.com>
2024-02-29 00:15:04 -0600

    updated approach after realizing there's not a need to create a new set from a since it is a set already

--- sets/unionOfTwo.js
+++ sets/unionOfTwo.js
@@ -1,10 +1,16 @@
-function myFunction(a, b){
-let set = new Set(a);
-let union = new Set([...set,...b]);
-    return union
+// Write a function that takes two Sets as arguments.
+// Create the union of the two sets. Return the result.
+// Hint: try not to switch to Arrays, this would slow down your code
+
+function myFunction1(a, b) {
+    return new Set([...a, ...b]);
 }
 
+// ! note: There is an experimental method for JS Sets: .union() it is not widely supported
+function myFunction2(a, b) {
+    return a.union(b)
+}
 
-myFunction(new Set('123'), new Set('234'))
-myFunction(new Set([1, 2, 3]), new Set([3, 4, 5]))
-myFunction(new Set([false, false, NaN]), new Set([true, true, NaN]))
+myFunction1(new Set('123'), new Set('234')) // Set(4) { '1', '2', '3', '4' }
+myFunction1(new Set([1, 2, 3]), new Set([3, 4, 5])) // Set(5) { 1, 2, 3, 4, 5 }
+myFunction1(new Set([false, false, NaN]), new Set([true, true, NaN])) // Set(3) { false, NaN, true }
