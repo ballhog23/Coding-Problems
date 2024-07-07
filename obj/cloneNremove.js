04bb9657423e16b805e18bae0e86381082c4d0e9
Caleb <blakeshawart@gmail.com>
2024-07-07 08:31:46 -0500

    reworked cloneNremove

--- obj/cloneNremove.js
+++ obj/cloneNremove.js
@@ -1,18 +1,34 @@
 // You are tasked with implementing a JavaScript function named cloneObject that takes two parameters:
 // an object (object) and an array of strings (removeProperties).
-// The goal of this function is to create a shallow copy of the input object and remove specified properties from the copy.
+
 
 function cloneObject(object, removeProperties) {
-    let obj = Object.assign({}, object) // creates a shallow copy of the object passed
 
-    removeProperties.forEach(i => obj.hasOwnProperty(i) ? delete obj[i] : null)
+    let obj = {};
 
-    return obj
+    // loop through properties of passed object
+    for (let key in object) {
+
+        // and if the property does not exist in the removeProperties array, push to new object
+        if (object.hasOwnProperty(key) && !removeProperties.includes(key)) {
+            obj[key] = object[key];
+        }
+    }
+
+    return obj;
 }
 
-cloneObject({ "apples": 7, "bananas": 42, "citrones": 20, "sausages": 2 }, ["sausages"])
-cloneObject({ "apples": 7, "bananas": 42, "citrones": 20, "sausages": 2 }, [])
+cloneObject(
+    { "apples": 7, "bananas": 42, "citrones": 20, "sausages": 2 },
+    ["sausages"]
+)
+
 cloneObject(
     { "name": "Iron Man", "age": 42, "superPower": "None", "friends": 0, "overweight": true, "hungry": true },
     ["overweight", "hungry"]
+)
+
+cloneObject(
+    { "a": 1, "z": 25, "h": 8, "i": 9, "d": 4, "u": 20, "p": 15 },
+    []
 )
