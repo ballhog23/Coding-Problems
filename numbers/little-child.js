f8b3772918d3b8cca78704f0154f5f3e61badd64
Caleb <blakeshawart@gmail.com>
2024-08-02 18:34:30 -0500

    little child description

--- numbers/little-child.js
+++ numbers/little-child.js
@@ -1,3 +1,7 @@
+// Given are two variables child1 and child2 with a numeric value between 0 and 99.
+// Return true if a child is between 0 and 14 (0 and 14 inclusive).
+// Return false if both or none in between.
+
 function littleChild(child1, child2) {
     if (child1 < 15 && child1 > -1 && child2 < 15 && child2 > -1) return false;
     else if (child1 < 15 && child1 > -1 || child2 < 15 && child2 > -1) return true;
