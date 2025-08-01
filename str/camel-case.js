172c5287121be9d2e0591bbd68d0240a881d4fc2
Caleb Pirkle <calebpirkle2@gmail.com>
2025-07-31 22:04:54 -0500

    format

--- str/camel-case.js
+++ str/camel-case.js
@@ -2,8 +2,8 @@ function camelCase(str) {
     const trimmed = str.trim();
     const explode = trimmed.split(' ');
     const toUpper = explode
-      .filter( i =>i !== '')
-      .map(i => i[0].toUpperCase() + i.slice(1, i.length))
+        .filter(i => i !== '')
+        .map(i => i[0].toUpperCase() + i.slice(1, i.length))
     const join = toUpper.join('');
     return join.charAt(0).toLowerCase() + join.slice(1, join.length)
 }
